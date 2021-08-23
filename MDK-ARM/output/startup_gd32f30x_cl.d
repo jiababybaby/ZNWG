e2b82e82944ac77dce2211d8209e30ef61c8cfa8
jiababybaby <1520170162@qq.com>
2021-08-23 23:32:57 +0800

    del

--- MDK-ARM/output/startup_gd32f30x_cl.d
+++
@@ -1,1 +0,0 @@
-.\output\startup_gd32f30x_cl.o: ..\..\..\..\GD32F30x_Firmware_Library\CMSIS\GD\GD32F30x\Source\ARM\startup_gd32f30x_cl.s
--- Soft_Drive/bsp_tim.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BSP_TIM_H
-#define _BSP_TIM_H
-
-void timer0_config(void);
-
-
-#endif
-
